c8cad4ecd54fd284078022dadac6269777edf5c8
Nelson Wells <nelsonwellswku@gmail.com>
2023-04-03 20:58:14 -0500

    Add new vite project to replace the CRA ui
    in addition to vite, also includes
      * pnpm for front-end
      * daisyui library
      * b2c integration including sending access token to backend

---
+++ ui/src/app-config.ts
@@ -0,0 +1,5 @@
+const exports = {
+    GRAPHQL_URI: 'http://localhost:4000/',
+}
+
+export default exports;
---
+++ ui/src/components/NavBar/index.tsx
@@ -0,0 +1,12 @@
+import { MagnifyingGlassIcon } from "../Icons";
+
+const NavBar = () => (
+    <div className="bg-slate-50">
+        <div className="navbar container mx-auto">
+            <div><h1 className="text-2xl">Twitter At Home</h1></div>
+        </div>
+    </div>
+
+);
+
+export default NavBar;
